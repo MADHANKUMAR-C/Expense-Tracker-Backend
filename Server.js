3a0a7c24bc27432cf54fabca1b7db2243d3d26ce
MADHANKUMAR-C <madhankumar8825487841@gmail.com>
2025-06-26 15:14:35 +0530

    Updated Commit

--- Server.js
+++ Server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const expenseRoutes = require('./routes/expenseRoutes').default;
+const connectDB = require('./config/db');
+const expenseRoutes = require('./routes/expenseRoutes');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+connectDB();
 app.use(express.json());
 app.use(cors());
 app.use('/api', expenseRoutes);
--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const mongooseUrl = 'mongodb+srv://madhankumar8825487841:mVdEf3n9XD8I7wCr@cluster0.mfe76lo.mongodb.net/CSD';
-mongoose.connect(mongooseUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('MongoDB connected successfully');
-}).catch((err) => {
-    console.error('MongoDB connection error:', err);
-});
-
-module.exports = mongoose;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongooseUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected successfully');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+};
+module.exports = connectDB;
