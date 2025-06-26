73b237980e7d4dce3949a3ef539f47a3956417a3
MADHANKUMAR-C <152647803+MADHANKUMAR-C@users.noreply.github.com>
2025-06-26 15:19:16 +0530

    Update expenseRoutes.js

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< main
-const { getExpenses, createExpense, updateExpense, deleteExpense } = require('../controllers/expenseController');
-=======
 const { getExpenses, createExpense, updateExpense, deleteExpense} = require('../controllers/expenseController');
->>>>>>> master
 router.get('/expenses', getExpenses);
 router.post('/expenses', createExpense);
 router.put('/expenses/:id', updateExpense);
 router.delete('/expenses/:id', deleteExpense);
-<<<<<<< main
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> master
